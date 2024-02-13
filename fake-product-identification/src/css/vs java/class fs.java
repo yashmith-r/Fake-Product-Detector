e6863487d10a1f0914b1e0c5ba4b186e86cd70e2
yashmith-r <yashmithraj_vemula@srmap.edu.in>
2024-02-13 12:36:37 +0530

    initial

---
+++ fake-product-identification/src/css/vs java/class fs.java
@@ -0,0 +1,7 @@
+public class fs
+{
+    public static void main(String args[])
+    {
+        System.out.println("heloo world");
+    }
+}
---
+++ fake-product-identification/tempCodeRunnerFile.java
@@ -0,0 +1,7 @@
+class fs
+{
+    public static voidf main(String args[])
+    {
+        System.out.println("heloo world");
+    }
+}
